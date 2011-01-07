930e6219d23220b7be72a837404d311ac2d91541
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-01-06 18:11:16 -0800

    Updated README for markdown compatibilty.

--- generate_readme.py
+++ generate_readme.py
@@ -14,8 +14,17 @@ s = SmartClient(SMART_SERVER_OAUTH['consumer_key'],
                        SMART_SERVER_PARAMS, 
                        SMART_SERVER_OAUTH)
 
+
 import pydoc
 t = pydoc.TextDoc()
 
+# TextDoc default behavior wrecks havoc with markdown interpretation.
+# So strip out the " |" line beginnings...
 sys.stdout = tmp
-print t.docclass(SmartClient)
+d = t.docclass(SmartClient)
+for l in d.split("\n"):
+  try:
+    if l[0:2] == " |":
+      l = "  " + l[2:]
+  except: pass
+  print l
