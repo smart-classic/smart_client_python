2640aa4cabb37d5eed29a2385dd24e40498b9792
Pascal Pfiffner <phase.of.matter@gmail.com>
2013-01-29 18:27:17 -0500

    Strip the " |" line beginnings in API documentation

--- generate_readme.py
+++ generate_readme.py
@@ -1,32 +1,40 @@
 import sys
-tmp = sys.stdout
-sys.stdout=sys.stderr
+import pydoc
+import re
+
 from client import SMARTClient
 
-CONSUMER_TOKENS = {'consumer_key': 'my-app@apps.smartplatforms.org', 
-                      'consumer_secret': 'smartapp-secret'}
+CONSUMER_TOKENS = {
+	'consumer_key': 'my-app@apps.smartplatforms.org',
+	'consumer_secret': 'smartapp-secret'
+}
 
 api_base = 'http://localhost:7000'
 
+tmp = sys.stdout
+sys.stdout = sys.stderr
 
-"""An important static var"""
+# instantiate our client and TextDoc 
 s = SMARTClient(api_base, CONSUMER_TOKENS)
-
-import pydoc
 t = pydoc.TextDoc()
+#t = pydoc.HTMLDoc()
 
-# TextDoc default behavior wrecks havoc with markdown interpretation.
-# So strip out the " |" line beginnings...
 sys.stdout = tmp
 d = t.docclass(SMARTClient)
 d = pydoc.plain(d)
+# TextDoc default behavior wrecks havoc with markdown interpretation.
+# So strip out the " |" line beginnings...
+d = re.sub(r"\n\s*\|", "\n", d)
+
 print """SMART Python Client Library
+===========================
 
 To generate this README:
 
-  $ python generate_readme.py > README
+    $ python generate_readme.py > README
 
 ---
 
 """
+
 print d
